9ac6ed64b3cab01b2e07bd53608f424f66c76df7
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-01-30 15:31:58 +0330

    Addresses mode added to the project.

--- Server/Addreses/models.py
+++ Server/Addreses/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from Accounts.models import User
 
-# Create your models here.
+
+
+class UserAddres(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_addres", verbose_name="کاربر"
+    )
+    
+    phone = models.CharField(
+        max_length=200, 
+        verbose_name="شماره تلقن"
+    )
+    
+    f_name = models.CharField(
+        max_length=200, 
+        verbose_name="نلم"
+    )
+    
+    l_name = models.CharField(
+        max_length=200,
+        verbose_name="نام خانوادگی"
+    )
+    
+    state = models.CharField(
+        max_length=200,
+        verbose_name="استان"
+    )
+    
+    city = models.CharField(
+        max_length=200,
+        verbose_name="شهر"
+    )
+    
+    addres = models.TextField(verbose_name="آدرس")
+    
+    plak = models.CharField(
+        max_length=200,
+        verbose_name="پلاک"
+    )
+    
+    unit = models.CharField(
+        max_length=200,
+        verbose_name="واحد"
+    )
+    
+    postal_code = models.CharField(
+        max_length=200, 
+        verbose_name="کد پستی"
+    )
+    
+    
+    class Meta:
+        verbose_name = "آدرس کاربر ها"
+        verbose_name_plural = "آدرس های کاربر ها"
