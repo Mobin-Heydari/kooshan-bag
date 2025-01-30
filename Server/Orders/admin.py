6394e40960205ca112a1f8fd258ee71e03b31a27
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-01-30 17:56:21 +0330

    Order admin is added to the project.

--- Server/Orders/admin.py
+++ Server/Orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'f_name', 'l_name', 
+                    'address', 'postal_code', 'city', 'paid',
+                    'created', 'updated',]
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
