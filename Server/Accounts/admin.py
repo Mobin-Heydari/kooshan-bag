ef4dbf7e6321bd5c5a16a1b57fd3a26cb217354c
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-01-30 10:10:44 +0330

    Users Admin is added.

--- Server/Accounts/admin.py
+++ Server/Accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User
 
-# Register your models here.
+
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "f_name", "l_name", "is_admin"]
+    search_fields = ["email"]
+    ordering = ["email"]
+    list_filter = ["is_admin"]
+    filter_horizontal = []
+
+
+admin.site.unregister(Group)
+
