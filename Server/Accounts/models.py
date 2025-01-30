6170b520aab027045c7863286f8fb5a07a17d6c7
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-01-30 10:07:08 +0330

    Auth model user is added to the project.

--- Server/Accounts/models.py
+++ Server/Accounts/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, f_name, l_name, password=None):
+        user = self.model(
+            email=self.normalize_email(email),
+            f_name=f_name,
+            l_name=l_name,
+            password=password
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, f_name, l_name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            f_name=f_name,
+            l_name=l_name,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="ایمیل",
+        max_length=255,
+        unique=True,
+    )
+    
+    f_name = models.CharField(verbose_name="نام",
+                              max_length=200, null=True)
+    l_name = models.CharField(verbose_name="نام خانوادگی",
+                              max_length=200, null=True)
+    
+    is_active = models.BooleanField(default=True,
+                                    verbose_name="فعال")
+    
+    is_admin = models.BooleanField(default=False,
+                                   verbose_name="ادمین")
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['f_name', 'l_name']
+    
+    
+    class Meta:
+        ordering = ['email']
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربر ها"
+        
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -130,3 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Auth User 
+AUTH_USER_MODEL = "Accounts.User"
