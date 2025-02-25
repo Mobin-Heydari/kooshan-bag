de5be9884733647a58f1341ebaac5148acb32d63
Mobin <mobinheydari.developer@gmail.com>
2025-02-25 02:27:51 -0800

    Login  and register authentication system is optimized and password validation system added and the system functionaled by using the error handeling system.

--- Server/Accounts/views.py
+++ Server/Accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.utils.crypto import get_random_string
 from django.urls import reverse
 from django.views.generic import View
@@ -29,51 +29,79 @@ class Userlogin(View):
             })
     
     def post(self, request):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['email'], password=cd['password'])
-            if user is not None:
-                login(request, user)
-                return redirect("/")
-        return render(request, 'account/login.html', {'form':form})
+        if not request.user.is_authenticated:
+            form = LoginForm(request.POST)
+
+            if form.is_valid():
+                cd = form.cleaned_data
+
+                print(cd['email'])
+                print(cd['password'])
+                
+                user = authenticate(username=cd['email'], password=cd['password'])
+
+                print(user)
+                
+                if user:
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    form.add_error('email', 'ایمیل یا رمز عبور معتبر نیست')
+            else:
+                form.add_error('email', 'ایمیل یا رمز عبور معتبر نیست')
+        else:
+            return redirect('/')
+        
+        primery_categories = PrimeryCategory.objects.all()
+        categories = Category.objects.all()
+        return render(request, 'account/login.html', {
+            'primery_categories' : primery_categories,
+            'categories' : categories,
+            'form' : form,
+        })
+
 
 
 class UserRegister(View):
-     def get(self, request):
-        if request.user.is_authenticated == True:
+    def get(self, request):
+        if request.user.is_authenticated:
             return redirect('/')
         else:
             primery_categories = PrimeryCategory.objects.all()
             categories = Category.objects.all()
             form = RegisterForm()
             return render(request, 'account/register.html', {
-                'primery_categories' : primery_categories,
-                'categories' : categories,
-                'form' : form,
+                'primery_categories': primery_categories,
+                'categories': categories,
+                'form': form,
             })
-     
 
-     def post(self, request):
+    def post(self, request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             if cd['password'] == cd['password_conf']:
-                if len(cd['password_conf']) <= 8 and len(cd['password_conf']) >= 8 :
-                    user = User.objects.create(
-                        email = cd['email'],
-                        password = cd['password'],
-                        f_name = cd['f_name'],
-                        l_name = cd['l_name']
-                    )
-                    login(request, user)
-                    return redirect("/")
-                else:
-                    form.add_error('password_conf', 'رمز عبور وارد شده کوچکتر از ۸ و یا بزرگتر از ۱۶ است')
-            else:
-                form.add_error('password_conf', 'رمز عبور خود را اشتباه وارد کردید')
+                if 8 <= len(cd['password_conf']) <= 16:
+                    if not User.objects.filter(email=cd['email']).exists():
 
-        return render(request, 'account/register.html', {'form':form})
+                        user = User.objects.create(
+                            email=cd['email'],
+                            f_name=cd['f_name'],
+                            l_name=cd['l_name']
+                        ) 
+                        user.set_password(cd['password'])
+                        user.save()
+                        login(request, user)
+                        return redirect("/")
+                    else:
+                        form.add_error('password', 'ایمل وارد شده قبلا استفاده شده')
+                else:
+                    form.add_error('password', 'رمز عبور باید بین ۸ تا ۱۶ کاراکتر باشد')
+            else:
+                form.add_error('password', 'رمز عبور خود را اشتباه وارد کردید')
+
+        return render(request, 'account/register.html', {'form': form})
+
 
 
 class UserLogout(View):
