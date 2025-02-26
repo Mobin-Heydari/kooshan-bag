9a9cfedbe206edfb1612c5a7543afd4a0014f0ec
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-02-26 03:02:05 -0800

    Urls added.

--- Server/Accounts/urls.py
+++ Server/Accounts/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('user-logout/', views.UserLogout.as_view(), name='user_logout'),
     path('user-register/', views.UserRegister.as_view(), name='user_register'),
     path('check-otp/', views.CheckOtp.as_view(), name='check_otp'),
+
+    path('reset-password/', views.ResetPassword.as_view(), name='reset_password'),
 ]
